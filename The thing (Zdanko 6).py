768eefac6b33ea04b06e5498d254458d7eb60294
PolyMiOs <old.headless.nick@gmail.com>
2024-11-07 11:18:37 +0100

    Update The thing (Zdanko 6).py

--- The thing (Zdanko 6).py
+++ The thing (Zdanko 6).py
@@ -1,12 +1,14 @@
-slow = input("Podaj ciąg znaków: ")
-n = len(slow)
-start = 0
-stop = 0
 def ladnyprint(A,B):
     n = len(A)
     print(" Zank ----> DecASCII ")
     for i in range(n):
         print(A[i],"---->",B[i])
+
+slow = input("Podaj ciąg znaków: ")
+n = len(slow)
+start = 0
+stop = 0
+
 
 for i in range(n):
     if slow[i] == '0':
@@ -17,11 +19,18 @@ for i in range(n-1, 1,-1):
     if slow[i] == '0':
         stop = i
         break
+        
 new_slow = list(set(slow[start:stop]))
+if len(new_slow) == 0:
+    print("Xera sa kollo siebie / jest 1 zero / zer  ni ma")
+    exit()
+    
 new_slow.sort(reverse=True)
-
 zanki = [ord(new_slow[i]) for i in range(len(new_slow))]
 
 ladnyprint(new_slow, zanki)
-print("Pionty co do wielkośći decASCII to:", new_slow[4])
+if len(new_slow) >=5:
+    print("Pionty co do wielkośći decASCII to:", new_slow[4])
+else:
+    print(";-; Piontke nie ma （πーπ)")
 
