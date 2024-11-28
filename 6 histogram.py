92b2f3d6a7c54f2a97703a3ee0e0779d38ae8155
PolyMiOs <old.headless.nick@gmail.com>
2024-11-28 15:11:41 +0100

    Commit 28/11 zajeciowy

---
+++ 6 histogram.py
@@ -0,0 +1,40 @@
+from random import randint
+from tabulate import tabulate
+
+
+def pizza(n,m,o):
+    base = [[[0]*o for _ in range(m)] for _ in range(n)]
+    count = [0] * 10000
+    for wys in range(n):
+        for szer in range(m):
+            for dl in range(o):
+                el = randint(0,9999)
+                base[wys][szer][dl] = el
+                count[el] += 1
+    print(tabulate(base))
+    return count
+
+
+def histogram(binersy, kaunter):
+    for biners in binersy:
+        przedzial = 10000 // biners
+        sumy = [0] * biners
+        for i in range(biners):
+            sumy[i] = sum(kaunter[0+i*przedzial+i:(i+1)*przedzial])
+        if max(sumy) < 50:
+            break
+        
+        
+    for i in range(biners):
+        sumy[i] = "=" * sumy[i]
+        print(f"[{0+i*przedzial+i} - {(i+1)*przedzial}]   {sumy[i]}")
+
+
+biners = [3,5,10,20,50,100]        
+n = int(input("Podaj n: "))
+m = int(input("Podaj m: "))
+o = int(input("Podaj o: "))
+
+kaunter = pizza(n,m,o)
+
+histogram(biners, kaunter)
