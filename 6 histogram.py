b0a3e6c31dd3c39092ee2290ec71c64b707aa61c
PolyMiOs <mort.fajny@gmail.com>
2024-12-05 16:47:19 +0100

    yis

--- 6 histogram.py
+++ 6 histogram.py
@@ -1,18 +1,26 @@
 from random import randint
 from tabulate import tabulate
+import matplotlib.pyplot as plt
+
+
 
 
 def pizza(n,m,o):
     base = [[[0]*o for _ in range(m)] for _ in range(n)]
     count = [0] * 10000
+    baza = [0] * (m*n*o)
+    h = 0
     for wys in range(n):
         for szer in range(m):
             for dl in range(o):
                 el = randint(0,9999)
                 base[wys][szer][dl] = el
                 count[el] += 1
+                baza[h] = el
+                h += 1
     print(tabulate(base))
-    return count
+    print
+    return (count,baza)
 
 
 def histogram(binersy, kaunter):
@@ -20,27 +28,37 @@ def histogram(binersy, kaunter):
         przedzial = 10000 // biners
         sumy = [0] * biners
         for i in range(biners):
-            x = 1
-            if not i:
-                x = 0
-            sumy[i] = sum(kaunter[0+i*przedzial+x:(i+1)*przedzial])
+            x = 0
+            if 10000%biners == 0 or i:
+                x = 1
+            sumy[i] = sum(kaunter[0+i*przedzial:(i+1)*(przedzial)-x])
         if max(sumy) < 50:
             break
         
-        
+    if max(sumy) > 50:
+        raise ValueError
+        exit()
     for i in range(biners):
-        x=1
-        if not i:
-            x = 0
+        x=0
+        if 10000%biners == 0 or i:
+            x = 1
         sumy[i] = "=" * sumy[i]
-        print(f"[{0+i*przedzial+x:^4} - {(i+1)*przedzial}]   {sumy[i]}")
+        print(f"[{0+i*przedzial:^4} - {(i+1)*(przedzial)-x:^4}]   {sumy[i]}")
+    return biners
+    
 
+def histogramplotlib(M,a):
+        
+        plt.hist(M, bins=a)
+        #plt.show()
 
 biners = [3,5,10,20,50,100]        
 n = int(input("Podaj n: "))
 m = int(input("Podaj m: "))
 o = int(input("Podaj o: "))
 
-kaunter = pizza(n,m,o)
+kaunter, baza = pizza(n,m,o)
 
-histogram(biners, kaunter)
+numbins = histogram(biners, kaunter)
+histogramplotlib(baza, numbins)
+plt.show()
